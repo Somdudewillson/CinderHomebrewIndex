3d8217040e1a0956456087d2fa18c25cf993268a
Somdudewillson <somdudewilson@gmail.com>
2022-12-12 20:56:22 -0700

    Added homebrew data definitions.

---
+++ src/app/models/HomebrewData.ts
@@ -0,0 +1,47 @@
+import { ConditionType, CreatureType, DamageType, HomebrewType, ItemRarity, ItemType, MovementTypes, SizeClass, SkillType, SpecialSense, SpellComponents, SpellLevel, SpellSchool, StatType } from "./HomebrewEnums";
+
+export class HomebrewData {
+    type: HomebrewType = HomebrewType.CLASS;
+    title: string = '';
+    author: string = '';
+    link: string = '';
+    tags: string[] = [];
+    content: string[] = [];
+}
+
+export class HomebrewItemData extends HomebrewData {
+  itemType: ItemType = ItemType.WONDEROUS_ITEM;
+  rarity: ItemRarity = ItemRarity.COMMON;
+  requiresAttunment: boolean = false;
+  hasCharges: boolean = false;
+}
+
+export class HomebrewMonsterSearchData extends HomebrewData {
+    creatureType: CreatureType = CreatureType.ABERRATION;
+    challengeRating: number = 0.;
+    armorClass: number = 0;
+    hitpoints: number = 0;
+    size: SizeClass = SizeClass.MEDIUM;
+    specialSenses: SpecialSense[] = [];
+    saveProficiencies: StatType[] = [];
+    skillProficiencies: SkillType[] = [];
+    conditionImmunities: ConditionType[] = [];
+    vulnerabilities: DamageType[] = [];
+    resistances: DamageType[] = [];
+    damageImmunities: DamageType[] = [];
+    legendary: boolean = false;
+    mythic: boolean = false;
+    hasLair: boolean = false;
+    movementTypes: MovementTypes[] = [];
+}
+
+export class HomebrewSpellSearchData extends HomebrewData {
+    spellLevel: SpellLevel = SpellLevel.CANTRIP;
+    school: SpellSchool = SpellSchool.ABJURATION;
+    saveTypes: StatType[] = [];
+    damageTypes: DamageType[] = [];
+    conditions: ConditionType[] = [];
+    components: SpellComponents[] = [];
+    concentration: boolean = false;
+    ritual: boolean = false;
+}
