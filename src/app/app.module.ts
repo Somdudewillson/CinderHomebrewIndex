0b5507d90be01e937eb6513faf2835eb98a1a34c
Somdudewillson <somdudewilson@gmail.com>
2022-12-13 09:11:10 -0700

    moved enum set input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomebrewSearchBarComponent } from './homebrew-search-bar/homebrew-search-bar.component';
-import { EnumSetInputComponent } from './homebrew-search-bar/enum-set-input/enum-set-input.component';
+import { EnumSetInputComponent } from './search_inputs/enum-set-input/enum-set-input.component';
 import { ItemSearchFieldsComponent } from './homebrew-search-bar/item-search-fields/item-search-fields.component';
 import { HomebrewSearchResultsComponent } from './homebrew-search-results/homebrew-search-results.component';
 import { EnhancedBooleanInputComponent } from './search_inputs/enhanced-boolean-input/enhanced-boolean-input.component';
