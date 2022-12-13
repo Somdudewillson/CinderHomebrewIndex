23cff063afece54fddda6f9cabce1fc5659e8bc2
Somdudewillson <somdudewilson@gmail.com>
2022-12-12 21:34:32 -0700

    Removed unnecessary logging.

--- src/app/homebrew-search-bar/homebrew-search-bar.component.ts
+++ src/app/homebrew-search-bar/homebrew-search-bar.component.ts
@@ -74,7 +74,6 @@ export class HomebrewSearchBarComponent {
     }
     resultList.sort((a,b)=>b.score-a.score);
 
-    console.log(resultList);
     this.newSearchResultsEvent.emit(resultList.map(sortableData=>sortableData.data));
   }
 
