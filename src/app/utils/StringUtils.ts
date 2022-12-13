6d4f8279d63b00d3eedff3ec141bd3ca35b0bdd0
Somdudewillson <somdudewilson@gmail.com>
2022-12-12 20:52:23 -0700

    Added string distance util.

---
+++ src/app/utils/StringUtils.ts
@@ -0,0 +1,9 @@
+const EditDistance = require('edit-distance');
+
+const insertCost = function(_: string) { return 1; };
+const removeCost = function(_: string) { return 1.25; };
+const updateCost = function(stringA: string, stringB: string) { return stringA !== stringB ? 1.15 : 0; };
+
+export function computeStringSimilarity(a: string, b: string): number {
+    return EditDistance.levenshtein(a,b,insertCost,removeCost,updateCost).distance;
+}
